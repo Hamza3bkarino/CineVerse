15e04d1655b035a2d651ceef53c3997282922c37
Hamza Abkarino <errajihamza80@gmail.com>
2025-11-17 16:21:24 +0100

    Structur of Project & HeroSection of Homa Page

---
+++ my-app/src/Pages/Home.jsx
@@ -0,0 +1,18 @@
+
+export default function Home (){
+    return(
+        <section className="HeroSectionHome">
+            <div className="heroContent">
+                <h1>CineVerse – Where Stories Come Alive</h1>
+                {/* <p>
+                    Discover the magic of cinema. Explore the latest blockbusters,
+                    timeless classics, and hidden gems – all in one place
+                </p> */}
+                
+                <button>
+                    Discover Movies
+                </button>
+            </div>
+        </section>
+    )
+}
