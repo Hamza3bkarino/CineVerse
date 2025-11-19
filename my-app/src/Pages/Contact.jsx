99cfabb5ca35dcc7e837ceae6446549e47148ef0
Hamza Abkarino <errajihamza80@gmail.com>
2025-11-19 10:25:29 +0100

    Create Top movies and all movies and category sections & contact page

--- my-app/src/Pages/Contact.jsx
+++ my-app/src/Pages/Contact.jsx
@@ -1,10 +1,122 @@
 
+import { useEffect, useState } from "react";
+
+
 export default function Contact (){
+    const [name,setName]=useState('')
+    const [email,setEmail]=useState('')
+    const [message,setMessage]=useState('')
+    const [alert,setAlert]=useState(false)
+
+
+    useEffect(() => {
+        let lastVisible = false; // track if element was visible last frame
+    
+        const handleScroll = () => {
+          const element = document.querySelector("#venomTitle");
+          if (!element) return;
+    
+          const rect = element.getBoundingClientRect();
+          const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
+    
+          // When it becomes visible (enter screen)
+          if (isVisible && !lastVisible) {
+            const audio = new Audio("../../public/venom_qXqDVYX.mp3");
+            audio.volume = 0.1;
+            audio.play();
+          }
+    
+          // update previous visibility state
+          lastVisible = isVisible;
+        };
+    
+        window.addEventListener("scroll", handleScroll);
+    
+        return () => window.removeEventListener("scroll", handleScroll);
+      }, []);
+
+    const handleClick = (e)=>{
+        e.preventDefault();
+        const nameError = document.getElementById('nameError')
+        const emailError = document.getElementById('emailError')
+        const messageError = document.getElementById('messageError')
+
+        let valid = true;
+        if(!name.trim()){
+            nameError.textContent='Name must be existe'
+            valid=false
+        }else{
+        
+            nameError.textContent=''
+        }
+        
+        if(!email.includes('@')){
+            emailError.textContent='Email must be valid'
+            valid=false
+        }else{
+            emailError.textContent=''
+        } 
+        if(!message.trim()){
+            messageError.textContent='message must be existe'
+            valid=false
+        }else{
+            messageError.textContent=''
+        }
+
+        if(valid){
+            setAlert(true)
+        }
+        
+    }
+
     return(
         <>
-            <h1>
-                Contact Page
-            </h1>
+            <section className="contactSection">
+                {
+                    alert &&(
+                        <div className="alert success">
+                            <span className="alert-message">Your message has sent successfully !</span>
+                            <button className="alert-close" onClick={() => setAlert(false)}>
+                                ×
+                            </button>
+                        </div>
+                    )
+                }
+                <div className="contactContainer">
+                    <h2>Contact Us</h2>
+                    <p>Have questions or suggestions? Drop us a message!</p>
+
+                    <form className="contactForm">
+                        <div className="formGroup">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" id="name" placeholder="Your Name" value={name} onChange={(e)=>setName(e.target.value)} />
+                            <p style={{fontSize:'10px',color:'red'}} id="nameError"></p>
+                        </div>
+
+                        <div className="formGroup">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" id="email" placeholder="Your Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                            <p style={{fontSize:'10px',color:'red'}} id="emailError"></p>
+
+                        </div>
+
+                        <div className="formGroup">
+                            <label htmlFor="message">Message</label>
+                            <textarea id="message" rows="5" placeholder="Your Message" value={message} onChange={(e)=>setMessage(e.target.value)}></textarea>
+                            <p style={{fontSize:'10px',color:'red'}} id="messageError"></p>
+                        </div>
+
+                        <div className="formGroup">
+                            <button onClick={(e)=>handleClick(e)} type="submit" className="sendBtn">
+                                Send Message
+                            </button>
+                        </div>
+                    </form>
+                </div>
+
+                <h1  id="venomTitle" className="venomBG">We Are Venome</h1>
+
+            </section>
         </>
     )
 }
---
+++ my-app/src/data/movies.ts
@@ -0,0 +1,244 @@
+export const movies = [
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    img: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    desc: "Batman faces his greatest enemy, The Joker, in a battle for Gotham’s soul.",
+    production: "Warner Bros.",
+    rate: 9.0,
+    categorie: "Action"
+  },
+  {
+    title: "Inception",
+    year: 2010,
+    img: "https://image.tmdb.org/t/p/w500/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg",
+    desc: "A thief who steals secrets through dreams must perform the impossible—plant an idea.",
+    production: "Legendary Pictures",
+    rate: 8.8,
+    categorie: "Sci-Fi"
+  },
+  {
+    title: "Interstellar",
+    year: 2014,
+    img: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
+    desc: "A team of explorers travels through a wormhole searching for a new home for humanity.",
+    production: "Syncopy",
+    rate: 8.6,
+    categorie: "Sci-Fi"
+  },
+  {
+    title: "Oppenheimer",
+    year: 2023,
+    img: "./Oppenheimer.webp",
+    desc: "The story of J. Robert Oppenheimer and the creation of the atomic bomb.",
+    production: "Universal Pictures",
+    rate: 8.6,
+    categorie: "Drama"
+  },
+  {
+    title: "Dune: Part Two",
+    year: 2024,
+    img: "./Dune Part Two.webp",
+    desc: "Paul Atreides rises as a leader as the war for Arrakis intensifies.",
+    production: "Legendary Pictures",
+    rate: 8.8,
+    categorie: "Sci-Fi"
+  },
+  {
+    title: "Avatar",
+    year: 2009,
+    img: "https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg",
+    desc: "A paraplegic Marine travels to Pandora and joins the Na’vi people.",
+    production: "20th Century Studios",
+    rate: 7.6,
+    categorie: "Adventure"
+  },
+  {
+    title: "Joker",
+    year: 2019,
+    img: "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+    desc: "The origin story of Arthur Fleck and his transformation into the Joker.",
+    production: "Warner Bros.",
+    rate: 8.4,
+    categorie: "Drama"
+  },
+  {
+    title: "The Batman",
+    year: 2022,
+    img: "https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg",
+    desc: "Batman investigates a series of murders and uncovers deep corruption.",
+    production: "Warner Bros",
+    rate: 8.1,
+    categorie: "Action"
+  },
+  {
+    title: "John Wick: Chapter 4",
+    year: 2023,
+    img: "https://image.tmdb.org/t/p/w500/vZloFAK7NmvMGKE7VkF5UHaz0I.jpg",
+    desc: "John Wick fights his way toward defeating the High Table.",
+    production: "Lionsgate",
+    rate: 8.1,
+    categorie: "Action"
+  },
+  {
+    title: "The Shawshank Redemption",
+    year: 1994,
+    img: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+    desc: "Two imprisoned men bond over years, finding solace and redemption through acts of common decency.",
+    production: "Columbia Pictures",
+    rate: 9.3,
+    categorie: "Drama"
+  },
+  {
+    title: "Forrest Gump",
+    year: 1994,
+    img: "./Forrest Gump.webp",
+    desc: "The life story of a simple man who witnesses and influences several defining historical events.",
+    production: "Paramount Pictures",
+    rate: 8.8,
+    categorie: "Drama"
+  },
+  {
+    title: "Pulp Fiction",
+    year: 1994,
+    img: "./Pulp Fiction.webp",
+    desc: "The lives of two mob hitmen, a boxer, a gangster's wife, and a pair of diner bandits intertwine.",
+    production: "Miramax Films",
+    rate: 8.9,
+    categorie: "Crime"
+  },
+  {
+    title: "The Lord of the Rings: The Return of the King",
+    year: 2003,
+    img: "./The Lord of the Rings_The Return of the King.webp",
+    desc: "Aragorn leads the world of men against Sauron's forces, while Frodo and Sam approach Mount Doom.",
+    production: "New Line Cinema",
+    rate: 8.9,
+    categorie: "Fantasy"
+  },
+  {
+    title: "Fight Club",
+    year: 1999,
+    img: "./Fight club.webp",
+    desc: "An insomniac office worker looking for a way to change his life crosses paths with a devil-may-care soap maker.",
+    production: "20th Century Fox",
+    rate: 8.8,
+    categorie: "Drama"
+  }
+];
+
+
+export const topMovies = [
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    img: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+    desc: "Batman faces his greatest enemy, The Joker, in a battle for Gotham’s soul.",
+    production: "Warner Bros.",
+    rate: 9.0,
+    categorie: "Action"
+  },
+  {
+    title: "Inception",
+    year: 2010,
+    img: "https://image.tmdb.org/t/p/w500/edv5CZvWj09upOsy2Y6IwDhK8bt.jpg",
+    desc: "A thief who steals secrets through dreams must perform the impossible—plant an idea.",
+    production: "Legendary Pictures",
+    rate: 8.8,
+    categorie: "Sci-Fi"
+  },
+  {
+    title: "Interstellar",
+    year: 2014,
+    img: "https://image.tmdb.org/t/p/w500/rAiYTfKGqDCRIIqo664sY9XZIvQ.jpg",
+    desc: "A team of explorers travels through a wormhole searching for a new home for humanity.",
+    production: "Syncopy",
+    rate: 8.6,
+    categorie: "Sci-Fi"
+  },
+  {
+    title: "Oppenheimer",
+    year: 2023,
+    img: "./Oppenheimer.webp",
+    desc: "The story of J. Robert Oppenheimer and the creation of the atomic bomb.",
+    production: "Universal Pictures",
+    rate: 8.6,
+    categorie: "Drama"
+  },
+  {
+    title: "Dune: Part Two",
+    year: 2024,
+    img: "./Dune Part Two.webp",
+    desc: "Paul Atreides rises as a leader as the war for Arrakis intensifies.",
+    production: "Legendary Pictures",
+    rate: 8.8,
+    categorie: "Sci-Fi"
+  },
+  {
+    title: "Avatar",
+    year: 2009,
+    img: "https://image.tmdb.org/t/p/w500/jRXYjXNq0Cs2TcJjLkki24MLp7u.jpg",
+    desc: "A paraplegic Marine travels to Pandora and joins the Na’vi people.",
+    production: "20th Century Studios",
+    rate: 7.6,
+    categorie: "Adventure"
+  },
+  {
+    title: "Joker",
+    year: 2019,
+    img: "https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg",
+    desc: "The origin story of Arthur Fleck and his transformation into the Joker.",
+    production: "Warner Bros.",
+    rate: 8.4,
+    categorie: "Drama"
+  },
+  {
+    title: "The Batman",
+    year: 2022,
+    img: "https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg",
+    desc: "Batman investigates a series of murders and uncovers deep corruption.",
+    production: "Warner Bros",
+    rate: 8.1,
+    categorie: "Action"
+  },
+  {
+    title: "John Wick: Chapter 4",
+    year: 2023,
+    img: "https://image.tmdb.org/t/p/w500/vZloFAK7NmvMGKE7VkF5UHaz0I.jpg",
+    desc: "John Wick fights his way toward defeating the High Table.",
+    production: "Lionsgate",
+    rate: 8.1,
+    categorie: "Action"
+  },
+  {
+    title: "The Shawshank Redemption",
+    year: 1994,
+    img: "https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+    desc: "Two imprisoned men bond over years, finding solace and redemption through acts of common decency.",
+    production: "Columbia Pictures",
+    rate: 9.3,
+    categorie: "Drama"
+  }
+];
+
+
+
+
+
+export const categories = [
+  { name: "Action", desc: "High-energy movies filled with fights, chases, and intense battles." ,img:'../public/Action_Movie.jpg'},
+  { name: "Adventure", desc: "Exciting journeys, exploration, and discovering unknown worlds." ,img:'../public/adventure_movie.jpg'},
+  { name: "Drama", desc: "Emotional and character-driven stories about real-life struggles." ,img:'../public/drama_movie.jpg'},
+  { name: "Comedy", desc: "Light-hearted movies designed to entertain and make you laugh." ,img:'../public/Comedy_movie.jpg'},
+  { name: "Horror", desc: "Scary films using suspense, fear, and supernatural elements." ,img:'../public/alien-romulus-3840x2160-18261.jpg'},
+  { name: "Thriller", desc: "Tense, suspenseful stories full of twists and mystery." ,img:'../public/the-gorge-2025-3840x2160-20243.jpg'},
+  { name: "Sci-Fi", desc: "Futuristic worlds with advanced technology, space, and science concepts." ,img:'../public/Sci-Fi.jfif'},
+  { name: "Fantasy", desc: "Magical worlds, mythical creatures, and supernatural powers." ,img:'../public/fantasy.jpg'},
+  { name: "Romance", desc: "Movies centered around love, relationships, and emotional connections.",img:'../public/Romance.jfif' },
+  { name: "Mystery", desc: "Stories focused on solving clues, crimes, and uncovering hidden secrets.",img:'../public/herror movie.jpg' },
+  { name: "Crime", desc: "Mafia, heists, investigations, and criminal underworld stories.",img:'../public/Crime.jpg' },
+  { name: "Animation", desc: "Animated movies with creative visuals for all ages.",img:'../public/wp9101972-animation-movie-4k-wallpapers.jpg' },
+  { name: "War", desc: "Films about soldiers, battles, and military missions.",img:'../public/Historical.jpg' },
+  { name: "Superhero", desc: "Heroes with powers fighting villains and saving the world.",img:'../public/Superhero.jfif' },
+  { name: "Anime", desc: "Japanese animated films with deep emotion and unique art style." ,img:'../public/anime.jpg'}
+];
